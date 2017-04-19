20cfa983f191da8f8a3ac8d2c0aeda859a4e0eba
Nandan Kumar 2 <nku102@sapient.com>
2017-04-19 17:38:35 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+var zomato = require('zomato');
+const curl = require('curlrequest');
+
+var client = zomato.createClient({
+  userKey: 'd9366021b4991b6c8dd1979c6bc17caf', //as obtained from [Zomato API](https://developers.zomato.com/apis) 
+});
+client.getCategories(null, function(err, result){
+    if(!err){
+      app.get('/categories', function (req, res) {
+         res.send(result);
+})
+    }else {
+      console.log(err);
+    }
+});
+
+
+app.use(express.static('./public'))
+
+
+
+/*To format Jive Response */
+app.listen(1337,(err) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    console.log('Listening at http://127.0.0.1:1337');
+});
